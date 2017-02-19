9eadb57dbf67e5c36fcb7743d8850bc998735aa4
shahenaz Monia <shahy.m.93@gmail.com>
2017-02-19 13:19:38 +0200

    make npm package

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-var fs = require('fs');
-var html = fs.readFile(__dirname+'index.html', function(err, data){
-  console.log(data);
-});
+
 
 var http = require('http');
 http.createServer(function(req,res){
-  res.end(html);
-}).listen(8080,function(){
-  //console.log('Listening on 8080');
+  res.end('shahenaz monia');
+}).listen(process.env.PORT||8080,function(){
+  console.log('Listening on 8080');
 });
